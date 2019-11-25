7a01cc8630e374eda5b668b1bf863defc5459101
valeria-ivasheva <valeria.ivasheva@outlook.com>
2019-11-25 21:53:58 +0300

    Added some authorization and registration files.Only the beginning.

---
+++ Shelter5/wwwroot/users/authAndRegistration.ts
@@ -0,0 +1,20 @@
+﻿$(function authorization () {
+    $("#auth").submit(function () {
+        var formData = {
+            "inputEmail": $("#inputPassword").val()
+            , "inputPassword": $("#inputPassword").val()
+        };
+        var jsonObject = JSON.stringify(formData);
+        $.ajax({
+            url: ' '
+            , type: 'POST'
+            , data: 'jsonData=' + jsonObject
+            , success: function (res) {
+                alert(res);
+                console.log($.parseJSON(res).username);
+	            console.log($.parseJSON(res).token);
+            }
+        });
+        return false;
+    });
+});
